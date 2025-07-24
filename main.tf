7089f54881d2850dbb039c60b2852398aebee2c8
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-24 21:16:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
 module "instance" {
   source = "../day6"
-  ami_id           = "ami-0a1235697f4afa8a4"  
-  instance_type    = "t2.micro"
-  instance_tags    = { 
-        Name       ="Dev-instance"  
-    }
+ 
 }
-
